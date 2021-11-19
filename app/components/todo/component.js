872d8d0a63ae4a203aacfd378fb51808abcd72be
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2021-11-19 14:51:15 +0100

    --wip-- [skip ci]

--- app/components/todo/component.js
+++ app/components/todo/component.js
@@ -1,3 +1,9 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class TodoComponent extends Component {}
+export default class TodoComponent extends Component {
+  @action
+  removeTodo() {
+    this.args.todo.destroyRecord();
+  }
+}
